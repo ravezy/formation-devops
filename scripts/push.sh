07296bbbd9871b0bdf9aae932db565a616b778da
Alice Bob <alice@eni.fr>
2018-10-05 10:25:15 +0200

    hello-http-get

---
+++ scripts/push.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+git remote add cleverapps https:////git@push-par-clevercloud-customers.services.clever-cloud.com/app_8571170b-5b90-4948-b673-ec3f9752e11c.git
+
+git push --force --verbose cleverapps master
